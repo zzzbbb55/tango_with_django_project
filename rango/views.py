15c7763a0f9d0ca0ca3819190540a9379b2e6802
zibei zhao <zhaozibei2019@163.com>
2020-02-17 06:09:59 +0000

    finish chapter 8

--- rango/views.py
+++ rango/views.py
@@ -28,7 +28,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
