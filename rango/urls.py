0648e34b25c2110b670264efb063f0c5ca509817
zibei zhao <zhaozibei2019@163.com>
2020-02-17 00:21:36 +0000

    finish chapter6, 2 failures remain

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,9 @@ app_name = 'rango'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('about/',views.method,name='method'),
+    path('about/',views.about,name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
+    path('page/<slug:page_name_slug>/',
+        views.show_page, name='show_page'),
 ]
